8fa2fb2d4ed4cfae89f12c481c17b108a03ccb4a
Jorge <jorgediazok@gmail.com>
2020-08-31 12:14:52 -0300

    created first card of the portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,10 +1,62 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Box,
+  Grid,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from '@material-ui/core';
 import Navbar from './Navbar';
+import project1 from '../assets/images/html-css-javascript-lg.jpg';
 
 const Portfolio = () => {
   return (
     <React.Fragment>
-      <Navbar />
+      <Box component="div">
+        <Navbar />
+        <Grid container justify="center" alignItems="center">
+          {/*Project 1*/}
+          <Grid item xs={12} sm={8} md={6}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  alt="Project 1"
+                  height="140"
+                  image={project1}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5">
+                    Project 1
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
+                    dolorem labore numquam reprehenderit optio deserunt
+                    perspiciatis necessitatibus deleniti fuga. Consequatur!
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Github
+                  </Button>
+                  <Button size="small" color="primary">
+                    Deploy
+                  </Button>
+                </CardActions>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        </Grid>
+      </Box>
     </React.Fragment>
   );
 };
