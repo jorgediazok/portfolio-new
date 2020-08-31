2b3ca798f5d9473d0af506bb90c07c54da3f8058
Jorge <jorgediazok@gmail.com>
2020-08-31 00:06:53 -0300

    header completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,53 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { Typography, Avatar, Grid, Box } from '@material-ui/core';
+import Typed from 'react-typed';
 import avatar from '../assets/images/avatar.png';
 
+//Css styles
+const useStyles = makeStyles((theme) => ({
+  avatar: {
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+    margin: theme.spacing(1),
+  },
+  title: {
+    color: 'tomato',
+  },
+  subtitle: {
+    color: 'tan',
+    marginBottom: '3rem',
+  },
+  typedContainer: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '100vw',
+    textAlign: 'center',
+    zIndex: 1,
+  },
+}));
+
 const Header = () => {
+  const classes = useStyles();
   return (
-    <Box>
-      <Avatar src={avatar} alt="Avatar"></Avatar>
-      <Typography variant="h4"></Typography>
+    <Box className={classes.typedContainer}>
+      <Grid container justify="center">
+        <Avatar className={classes.avatar} src={avatar} alt="Avatar"></Avatar>
+      </Grid>
+      <Typography className={classes.title} variant="h5">
+        <Typed strings={['Jorge Díaz']} typeSpeed={40} />
+      </Typography>
+      <br />
+      <Typography className={classes.subtitle} variant="h5">
+        <Typed
+          strings={[' Web Design ', 'Web Development', 'MERN Stack']}
+          typeSpeed={60}
+          backSpeed={60}
+          loop
+        />
+      </Typography>
     </Box>
   );
 };
