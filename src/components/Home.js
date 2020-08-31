f1ab5df0ae6356628e64c34b679ec000bdc6b24e
Jorge <jorgediazok@gmail.com>
2020-08-31 00:27:26 -0300

    added particles canva

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Header from '../components/Header';
+import Particles from 'react-particles-js';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  particlesCanva: {
+    position: 'absolute',
+  },
+});
 
 const Home = () => {
+  const classes = useStyles();
   return (
     <div>
       <Navbar />
       <Header />
+      <Particles
+        canvasClassName={classes.particlesCanva}
+        params={{
+          particles: {
+            number: { value: 45, density: { enable: true, value_area: 600 } },
+          },
+        }}
+      />
     </div>
   );
 };
