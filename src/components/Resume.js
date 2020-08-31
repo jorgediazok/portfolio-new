f5126c56078696bdab051e67f481a0f3f531d014
Jorge <jorgediazok@gmail.com>
2020-08-31 02:00:54 -0300

    added styling in the resume but still need to find the bug

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,7 +1,123 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography, Box } from '@material-ui/core';
+import Navbar from './Navbar';
+
+const useStyles = makeStyles((theme) => ({
+  mainContainer: {
+    background: '#233',
+  },
+  timeLine: {
+    position: 'relative',
+    padding: '1rem',
+    margin: '0 auto',
+    '&:before': {
+      content: "''",
+      position: 'absolute',
+      height: '100%',
+      boder: '1px solid tan',
+      right: '40px',
+      top: 0,
+    },
+    '&:after': {
+      content: "''",
+      display: 'table',
+      clear: 'both',
+    },
+    [theme.breakpoints.up('md')]: {
+      padding: '2rem',
+      '&:before': {
+        left: 'calc(50% - 1px)',
+        right: 'auto',
+      },
+    },
+  },
+  timeLineItem: {
+    padding: '1rem',
+    borderBottom: '2px solid tan',
+    position: 'relative',
+    margin: '1rem 3rem 1rem 1rem',
+    clear: 'both',
+    '&:after': {
+      content: "''",
+      position: 'absolute',
+    },
+    '&:before': {
+      content: "''",
+      position: 'absolute',
+      right: '-0.625rem',
+      top: 'calc(50% - 5px)',
+      borderStyle: 'solid',
+      borderColor: 'tomato tomato transparent transparent',
+      borderWidth: '0.625rem',
+      transform: 'rotate(45deg)',
+    },
+    [theme.breakpoints.up('md')]: {
+      width: '44%',
+      margin: '1rem',
+      '&:nth-of-type(2n)': {
+        float: 'right',
+        margin: '1rem',
+        borderColor: 'tan',
+      },
+      '&:nth-of-type(2n):before': {
+        right: 'auto',
+        left: '-0.625rem',
+        borderColor: 'transparent transparent tomato tomato',
+      },
+    },
+  },
+  timeLineYear: {
+    textAlign: 'center',
+    maxWidth: '9.375rem',
+    margin: '0 3rem 0 auto',
+    fontSize: '1.8rem',
+    background: 'tomato',
+    color: 'white',
+    lineHeight: 1,
+    padding: '0.5rem 0 1rem',
+    '&:before': {
+      display: 'none',
+    },
+    [theme.breakpoints.up('md')]: {
+      textAlign: 'center',
+      margin: '0 auto',
+      '&:nth-of-type(2n)': {
+        float: 'none',
+        margin: '0 auto',
+      },
+      '&:nth-of-type(2n):before': {
+        display: 'none',
+      },
+    },
+  },
+  heading: {
+    color: 'tomato',
+    padding: '3rem 0',
+    textTransform: 'uppercase',
+  },
+}));
 
 const Resume = () => {
-  return <div>Hello from Resume Component</div>;
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <Navbar />
+      <Box component="header" className={classes.mainContainer}>
+        <Typography variant="h4" align="center" className={classes.heading}>
+          Working Experience
+        </Typography>
+        <Box component="div" className={classes.timeLine}>
+          <Typography
+            variant="h2"
+            className={`${classes.timeLineYear} ${classes.timeLineItem} `}
+          >
+            2020
+          </Typography>
+        </Box>
+      </Box>
+    </React.Fragment>
+  );
 };
 
 export default Resume;
