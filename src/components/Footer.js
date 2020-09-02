49a7d75526d8722b8f85797299d587fde3938029
Jorge <jorgediazok@gmail.com>
2020-09-02 03:07:46 -0300

    base portfolio done. New adds later

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,22 @@ import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
 import Facebook from '@material-ui/icons/Facebook';
-import Twitter from '@material-ui/icons/Twitter';
 import GitHub from '@material-ui/icons/GitHub';
 import Linkedin from '@material-ui/icons/LinkedIn';
 
 const useStyles = makeStyles({
-  '& .MuiBottomNavigationAction-root': {
-    minWidth: 0,
-    maxWidth: 250,
+  root: {
+    '& .MuiBottomNavigationAction-root': {
+      minWidth: 0,
+      maxWidth: 250,
+    },
+    '& .MuiSvgIcon-root': {
+      fill: 'tan',
+      '&:hover': {
+        fill: 'tomato',
+        fontSize: '1.8rem',
+      },
+    },
   },
 });
 
@@ -20,22 +28,17 @@ const Footer = () => {
     <React.Fragment>
       <BottomNavigation width="auto" style={{ background: '#222' }}>
         <BottomNavigationAction
-          className="classes.root"
+          className={classes.root}
           style={{ padding: '0' }}
           icon={<Facebook />}
         ></BottomNavigationAction>
         <BottomNavigationAction
-          className="classes.root"
-          style={{ padding: '0' }}
-          icon={<Twitter />}
-        ></BottomNavigationAction>
-        <BottomNavigationAction
-          className="classes.root"
+          className={classes.root}
           style={{ padding: '0' }}
           icon={<GitHub />}
         ></BottomNavigationAction>
         <BottomNavigationAction
-          className="classes.root"
+          className={classes.root}
           style={{ padding: '0' }}
           icon={<Linkedin />}
         ></BottomNavigationAction>
