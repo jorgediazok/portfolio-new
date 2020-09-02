ee48015c3dc5a77def3689de1f0242728f368c1b
Jorge <jorgediazok@gmail.com>
2020-09-01 23:18:24 -0300

    styling the form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,6 +3,16 @@ import { makeStyles, withStyles } from '@material-ui/core/styles';
 import { TextField, Typography, Button, Grid, Box } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
 import Navbar from './Navbar';
+import { Translate } from '@material-ui/icons';
+
+const useStyles = makeStyles((theme) => ({
+  form: {
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    position: 'absolute',
+  },
+}));
 
 const InputField = withStyles({
   root: {
@@ -27,17 +37,19 @@ const InputField = withStyles({
 })(TextField);
 
 const Contact = () => {
+  const classes = useStyles();
   return (
     <React.Fragment>
       <Navbar />
       <Box component="div">
         <Grid container justify="center">
-          <Box component="form">
+          <Box component="form" className={classes.form}>
             <Typography variant="h5">Contact me !</Typography>
             <InputField
               fullWidth={true}
               label="Name"
               variant="outlined"
+              inputProps={{ style: { color: 'white' } }}
               margin="dense"
               size="medium"
             ></InputField>
@@ -46,6 +58,7 @@ const Contact = () => {
               fullWidth={true}
               label="Email"
               variant="outlined"
+              inputProps={{ style: { color: 'white' } }}
               margin="dense"
               size="medium"
             ></InputField>
@@ -54,9 +67,14 @@ const Contact = () => {
               fullWidth={true}
               label="Country"
               variant="outlined"
+              inputProps={{ style: { color: 'white' } }}
               margin="dense"
               size="medium"
             ></InputField>
+            <br />
+            <Button variant="outlined" fullWidth={true} endIcon={<SendIcon />}>
+              Contact
+            </Button>
           </Box>
         </Grid>
       </Box>
