89fef4d152bda83aa650e2d6f9e6b681dfe04941
Jorge <jorgediazok@gmail.com>
2021-01-19 15:36:18 -0300

    modified the route of sendfile try 1

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.use(cors());
 //Check
 app.use(express.static('client/build/'));
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 //Route for form
